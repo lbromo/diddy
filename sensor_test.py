d933cc4fb541dc8a992f1aa4e0f2ce3b10f1cd48
Lasse <lbromo12@student.aau.dk>
2015-02-02 22:18:44 +0100

    updated values

--- sensor_test.py
+++ sensor_test.py
@@ -9,5 +9,6 @@ while(True):
     if(ts.is_pushed):
         motor.reset()
         motor.run_position_limited(90, 1000)
-        time.sleep(1)
+        time.sleep(5)
         motor.run_position_limited(-90, 1000)
+        time.sleep(5)
