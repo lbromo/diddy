b6048c41e2b1bae8b715ae6600bcf6968fce8644
Joakim Børlum <joakim@roevhat.dk>
2015-02-03 14:59:31 +0100

    Vi prøver noget nu

--- diddy.py
+++ diddy.py
@@ -6,35 +6,29 @@
 
 from ev3dev import *
 from ev3dev_utils.motors import *
-from observable import *
-
-class MotorController(observer):
-    def __init__(self, motorLeft, motorRight):
-        self.motorLeft = motorLeft
-        self.motorRight = motorRight
-
-    def update(*args, **kwargs):
-        if(type):
-            pass
-
 
 # SETUP MOTORS
-motorRight = dc_motor(OUTPUT_A)
-motorLeft  = dc_motor(OUTPUT_B)
+#motorRight = dc_motor(OUTPUT_A)
+#motorLeft  = dc_motor(OUTPUT_B)
 
 # SETUP TOUCH SENSORS
-frontTouchSensor = touch_sensor(INPUT_1)
-backTouchSensor  = touch_sensor(INPUT_2)
+#frontTouchSensor = touch_sensor(INPUT_1)
+#backTouchSensor  = touch_sensor(INPUT_2)
 
 # SETUP COLOR SENSOR
 colorSensor = color_sensor(INPUT_3)
+colorSensor.mode = color_sensor.mode_reflect
+blackLimit = 75
 
 # SETUP GYRO
 gyroSensor = gyro_sensor(INPUT_4)
 
-
 def runRandomly(direction = 0):
-    drive_for(motorLeft, motorRight, dir = direction, power = 100)
+    #drive_for(motorLeft, motorRight, dir = direction, power = 100)
 
 while(True)
+    if colorSensor.value() > blackLimit:
+        #TURN 180 deg
+        print 'DREEEJJJJ'
+
     runRandomly()
