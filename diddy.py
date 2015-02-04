ae3e9742e193319f6bff0792b86c15e18d802528
Joakim Børlum <joakim@roevhat.dk>
2015-02-04 13:29:04 +0100

    ...

--- diddy.py
+++ diddy.py
@@ -57,12 +57,12 @@ def cornerDetected():
         return False
 
 def turnRight():
-    motorRight.run_forever(-30)
-    motorLeft.run_forever(-30)
-    sleep(0.25)
+    #motorRight.run_forever(-30)
+    #motorLeft.run_forever(-30)
+    #sleep(0.25)
     motorRight.run_forever(-50)
     motorLeft.run_forever(50)
-    sleep(0.5)
+    sleep(0.63)
     #while colorSensor.reflect > MAGIC_NUMBER:
     #    motorRight.run_forever(-50)
     #    motorLeft.run_forever(50)
