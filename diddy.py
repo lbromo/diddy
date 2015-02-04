94226e3e2f2673f6334fcccea5befaa4bc3f4e65
Joakim Børlum <joakim@roevhat.dk>
2015-02-04 13:24:15 +0100

    ...

--- diddy.py
+++ diddy.py
@@ -62,7 +62,7 @@ def turnRight():
     sleep(0.2)
     motorRight.run_forever(-50)
     motorLeft.run_forever(50)
-    sleep(1)
+    sleep(0.63)
     #while colorSensor.reflect > MAGIC_NUMBER:
     #    motorRight.run_forever(-50)
     #    motorLeft.run_forever(50)
